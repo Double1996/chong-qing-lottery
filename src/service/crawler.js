29eb6841064baa3d10eca9f8a5a0f0099551f2aa
双磊(double) <sl3@meitu.com>
2018-09-12 23:47:50 +0800

    add little fix

--- src/service/crawler.js
+++ src/service/crawler.js
@@ -1,3 +1,9 @@
 module.exports = {
 
 }
+
+const webdriverio = require('webdriverio')
+var options = {
+  hosts = "localhost"
+
+}
--- src/service/login.js
+++ src/service/login.js
@@ -1,17 +1,3 @@
-const header = {
-
-}
-
-// 登录功能
-function AutoLogin(account) {
-  account = this.account
-
-  this.cookies = {
-    value = null,
-  }
-
-}
-
-function name(params) {
+export default class Login {
 
 }
