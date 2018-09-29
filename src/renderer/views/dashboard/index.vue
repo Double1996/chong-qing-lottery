6a7d31c5715c58ef914905347b15df739a988f31
双磊(double) <sl3@meitu.com>
2018-09-29 13:17:26 +0800

    init

---
+++ src/renderer/views/dashboard/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="dashboard-container">
+    <div class="dashboard-text">亲爱的用户{{name}}欢迎使用茗彩网下注客户端</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'dashboard',
+  computed: {
+    ...mapGetters([
+      'name',
+      'roles'
+    ])
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.dashboard {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
---
+++ src/renderer/views/form/index.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="app-container">
+    <el-form ref="form" :model="form" label-width="120px">
+      <el-form-item label="下注任务名称">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="下注使用金额">
+        <el-input v-model="form.money"></el-input>
+      </el-form-item>
+      <el-form-item label="下注出现策略">
+        <el-select v-model="form.region" placeholder="please select your zone">
+          <el-option label="大大大大小小小小" value="shanghai"></el-option>
+          <el-option label="小小小小大大大大" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="执行开始时间">
+        <el-col :span="11">
+          <el-date-picker type="date" placeholder="Pick a date" v-model="form.date1" style="width: 100%;"></el-date-picker>
+        </el-col>
+        <el-col class="line" :span="2">-</el-col>
+        <el-col :span="11">
+          <el-time-picker type="fixed-time" placeholder="Pick a time" v-model="form.date2" style="width: 100%;"></el-time-picker>
+        </el-col>
+      </el-form-item>
+      <el-form-item label="Instant delivery">
+        <el-switch v-model="form.delivery"></el-switch>
+      </el-form-item>
+      <el-form-item label="Activity type">
+        <el-checkbox-group v-model="form.type">
+          <el-checkbox label="Online activities" name="type"></el-checkbox>
+          <el-checkbox label="Promotion activities" name="type"></el-checkbox>
+          <el-checkbox label="Offline activities" name="type"></el-checkbox>
+          <el-checkbox label="Simple brand exposure" name="type"></el-checkbox>
+        </el-checkbox-group>
+      </el-form-item>
+      <el-form-item label="Resources">
+        <el-radio-group v-model="form.resource">
+          <el-radio label="Sponsor"></el-radio>
+          <el-radio label="Venue"></el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="下注备注">
+        <el-input type="textarea" v-model="form.desc"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">创建</el-button>
+        <el-button @click="onCancel">取消</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: '',
+        money: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      this.$message('下注成功~')
+    },
+    onCancel() {
+      this.$message({
+        message: '取消创建',
+        type: 'warning'
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.line{
+  text-align: center;
+}
+</style>
+
